aa6b18ee5f36326a37a146d880a369140c6b049f
superman566 <guochaohetony@gmail.com>
2022-10-16 15:20:58 -0400

    add more bugs

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,14 +3,15 @@ import './card.styles.css';
 export default function Card(props){
   const {
     id, 
-    name, 
-    email
+    name,
+    phone
   } = props.monster;
+  console.log(props.monster);
   return(
     <div className={'card-container'}>
-      <img alt={name} src={`https://robohash.org/${id}?set=set2&180*180`} />
+      <img alt={name} src={`https://robohash1.org/${id}?set=set2&180*180`} />
       <h2>{name}</h2>
-      <h3>{email}</h3>
+      <h3>{phone}</h3>
     </div>
   );
 }
