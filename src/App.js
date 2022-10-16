9301a2925af0b7ef29b3a6d9e9e86738a94d38ea
Guochao-He <guochao.he@ibm.com>
2022-10-16 11:28:16 -0400

    add style css files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log('render')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input 
+      className='search-box' 
+      type='search' 
+      placeholder='Search Monsters' 
+      onChange={(event)=> {
+        console.log(event);
+      }}
+      />
     </div>
   );
 }
